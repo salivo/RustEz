6b47559330927b4eaf19ead6cafc6108f67662f0
Name Not Found <volodymyr.sobol.s@gmail.com>
2025-10-19 19:17:24 +0200

    Update player.py
    merge in player

--- src/player.py
+++ src/player.py
@@ -3,12 +3,6 @@ from globals import PLAYER_SPEED
 
 
 class Player(entity.Entity):
-<<<<<<< HEAD
     def __init__(self, x: int = 0, y: int = 0):
         super().__init__(x, y)
-        self.speed: int = 5
-=======
-    def __init__(self):
-        super().__init__()
         self.speed: int = PLAYER_SPEED
->>>>>>> af88e1b (mobs brains)
