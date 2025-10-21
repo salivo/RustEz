63fcdb4d17b9c04e83765f2e6f323507764ad3c7
Lostuser <volodymyr.sobol.s@gmail.com>
2025-10-21 08:59:03 +0200

    lvl 3 add

---
+++ src/levels/level3.py
@@ -0,0 +1,10 @@
+import os
+import json
+from map import Map
+
+_here = os.path.dirname(__file__)
+
+with open(os.path.join(_here, "level3.gg")) as f:
+    data = json.load(f)  # pyright: ignore[reportAny]
+map = Map()
+map.tiles = data["map"]
